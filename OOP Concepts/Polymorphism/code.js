46b8b55a4c96c03417f8473bce4777a99e430b98
Raihan Uddin <uddinraihan797@gmail.com>
2024-08-15 20:30:42 +0600

    compile vs runtime polymorphism

--- OOP Concepts/Polymorphism/code.js
+++ OOP Concepts/Polymorphism/code.js
@@ -1,15 +1,25 @@
-class Car {
-  openDoor() {
-    console.log("The door has opened.");
+class MathFn {
+  sum(...inputs) {
+    switch (
+      arguments.length // or we can use "inputs.length"
+    ) {
+      case 2: {
+        const [a, b] = inputs;
+        return a + b;
+      }
+      case 3: {
+        const [a, b, c] = inputs;
+        return a + b + c;
+      }
+      case 4: {
+        const [a, b, c, d] = inputs;
+        return a + b + c + d;
+      }
+    }
   }
 }
 
-class ModernCar extends Car {
-  openDoor() {
-    super.openDoor();
-    console.log("Air is flowing nicely!");
-  }
-}
-
-const modernCar = new ModernCar();
-modernCar.openDoor(); //Air is flowing nicely.
+const math = new MathFn();
+console.log(math.sum(3, 4)); // 7
+console.log(math.sum(3, 4, 3)); // 10
+console.log(math.sum(1, 4, 3, 5)); // 13
