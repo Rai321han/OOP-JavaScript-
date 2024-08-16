ee1748734d158f09aa10eb7a32c7b50cf6f34568
Raihan Uddin <uddinraihan797@gmail.com>
2024-08-16 22:57:49 +0600

    update content of abstraction

--- OOP Concepts/Abstraction/code.js
+++ OOP Concepts/Abstraction/code.js
@@ -34,58 +34,87 @@
 // bike.calculateFuelEfficiency(); // Fuel Efficiency: 90%
 // bike.turnOnHeadlights(); // Error: This method should be defined in subclass
 
-class KFC {
-  constructor(branchName) {
-    if (this.constructor === KFC) {
-      throw new Error("Cannot instantiate abstract class KFC directly.");
-    }
-    this.branchName = branchName;
-  }
+// class KFC {
+//   constructor(branchName) {
+//     if (this.constructor === KFC) {
+//       throw new Error("Cannot instantiate abstract class KFC directly.");
+//     }
+//     this.branchName = branchName;
+//   }
 
-  // Common method for all branches, representing the specific recipe style
-  prepareRecipe() {
-    console.log(
-      `Preparing food using the secret KFC recipe at ${this.branchName} branch.`
-    );
-  }
+//   // Common method for all branches, representing the specific recipe style
+//   prepareRecipe() {
+//     console.log(
+//       `Preparing food using the secret KFC recipe at ${this.branchName} branch.`
+//     );
+//   }
 
-  // Abstract method for food delivery, to be implemented by each branch
-  deliverFood() {
-    throw new Error(
-      "Abstract method 'deliverFood' must be implemented by subclass."
-    );
+//   // Abstract method for food delivery, to be implemented by each branch
+//   deliverFood() {
+//     throw new Error(
+//       "Abstract method 'deliverFood' must be implemented by subclass."
+//     );
+//   }
+// }
+
+// // Urban branch of KFC with its own delivery style
+// class UrbanKFC extends KFC {
+//   constructor(branchName) {
+//     super(branchName);
+//   }
+
+//   // Implement the delivery style for the urban branch
+//   deliverFood() {
+//     console.log(`Delivering food by bike at ${this.branchName} branch.`);
+//   }
+// }
+
+// // Suburban branch of KFC with its own delivery style
+// class SuburbanKFC extends KFC {
+//   constructor(branchName) {
+//     super(branchName);
+//   }
+
+//   // Implement the delivery style for the suburban branch
+//   deliverFood() {
+//     console.log(`Delivering food by car at ${this.branchName} branch.`);
+//   }
+// }
+
+// const urbanKFC = new UrbanKFC("Urban KFC");
+// const suburbanKFC = new SuburbanKFC("Suburban KFC");
+
+// urbanKFC.prepareRecipe(); // Common recipe style
+// urbanKFC.deliverFood(); // Branch-specific delivery style
+
+// suburbanKFC.prepareRecipe(); // Common recipe style
+// suburbanKFC.deliverFood(); // Branch-specific delivery style
+
+class Vehicle {
+  constructor(type) {
+    if (this.constructor === Vehicle)
+      throw new Error("Cannot create instance of Interface.");
+    if (this.start === undefined)
+      throw new Error("Must override method 'start'.");
+    if (this.stop === undefined)
+      throw new Error("Must override method 'stop'.");
+
+    this.type = type;
   }
 }
 
-// Urban branch of KFC with its own delivery style
-class UrbanKFC extends KFC {
-  constructor(branchName) {
-    super(branchName);
+class Bike extends Vehicle {
+  constructor(type) {
+    super(type);
   }
 
-  // Implement the delivery style for the urban branch
-  deliverFood() {
-    console.log(`Delivering food by bike at ${this.branchName} branch.`);
-  }
+  // start() {
+  //   console.log(`${this.type} has started`);
+  // }
+
+  // stop() {
+  //   console.log(`${this.type} has stopped`);
+  // }
 }
 
-// Suburban branch of KFC with its own delivery style
-class SuburbanKFC extends KFC {
-  constructor(branchName) {
-    super(branchName);
-  }
-
-  // Implement the delivery style for the suburban branch
-  deliverFood() {
-    console.log(`Delivering food by car at ${this.branchName} branch.`);
-  }
-}
-
-const urbanKFC = new UrbanKFC("Urban KFC");
-const suburbanKFC = new SuburbanKFC("Suburban KFC");
-
-urbanKFC.prepareRecipe(); // Common recipe style
-urbanKFC.deliverFood(); // Branch-specific delivery style
-
-suburbanKFC.prepareRecipe(); // Common recipe style
-suburbanKFC.deliverFood(); // Branch-specific delivery style
+const bike = new Bike("Bike");
